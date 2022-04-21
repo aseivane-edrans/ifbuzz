da6eebd4c5e50b63884b40b3ce40ae501a274db1
Andy <aseivane@edrans.com>
2022-04-21 14:56:41 -0300

    First commit

---
+++ ifbuzz.py
@@ -0,0 +1,15 @@
+FIZZ = 1
+BUZZ = 2
+FIZZBUZZ = 3
+
+for i in range(1,100):
+    j=0
+    if(i%3 == 0): j=FIZZ
+    if(i%5 == 0): j=BUZZ
+    if(i%3 == 0 and i%5 == 0): j=FIZZBUZZ
+
+    match j:
+        case 0: print(i)
+        case 1: print("Fizz")
+        case 2: print("Buzz")
+        case 3: print("FizzBuzz") 
