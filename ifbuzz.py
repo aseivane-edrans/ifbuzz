6fdfc99b01976cde3cd58816796306e4a66ea960
Andy <aseivane@edrans.com>
2022-04-21 16:59:53 -0300

    def init

--- ifbuzz.py
+++ ifbuzz.py
@@ -2,14 +2,17 @@ FIZZ = 1
 BUZZ = 2
 FIZZBUZZ = 3
 
-for i in range(1,100):
-    j=0
-    if(i%3 == 0): j=FIZZ
-    if(i%5 == 0): j=BUZZ
-    if(i%3 == 0 and i%5 == 0): j=FIZZBUZZ
+def init():
+    for i in range(1,100):
+        j=0
+        if(i%3 == 0): j=FIZZ
+        if(i%5 == 0): j=BUZZ
+        if(i%3 == 0 and i%5 == 0): j=FIZZBUZZ
 
-    match j:
-        case 0: print(i)
-        case 1: print("Fizz")
-        case 2: print("Buzz")
-        case 3: print("FizzBuzz") 
+        match j:
+            case 0: print(i)
+            case 1: print("Fizz")
+            case 2: print("Buzz")
+            case 3: print("FizzBuzz") 
+
+init()
